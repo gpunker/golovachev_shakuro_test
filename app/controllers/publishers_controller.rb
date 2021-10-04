4c9462a967aa36904dbade13e36ab167cdb8f748
Alexandr Golovachev <gpunker@gmail.com>
2021-10-04 05:07:00 +0000

    give books in shops by a specific publisher

--- app/controllers/publishers_controller.rb
+++ app/controllers/publishers_controller.rb
@@ -10,9 +10,19 @@ class PublishersController < ApplicationController
 
   # Show shops which sells at least one book of specific publisher
   def show
-    @shops = Shop.select(:'shops.id', :'shops.name', :'books.id', :'books.title', :'stores.amount').
-                  joins(stores: [{ book: :publisher }]).
-                  where('publishers.slug' => params[:slug]).distinct
+    # @shops = Shop.select(:'shops.id', :'shops.name', :'books.id', :'books.title', :'stores.amount').
+    #               joins(stores: [{ book: :publisher }]).
+    #               where('publishers.slug' => params[:slug]).distinct
+    @shops = Shop.connection.select_all("
+      SELECT sh.id as shop_id, sh.name, b.id as book_id, b.title, st.amount
+      FROM shops sh
+      inner join stores st on sh.id = st.shop_id
+      inner join books b on b.id = st.book_id
+      inner join publishers p on p.id = b.publisher_id
+      where p.slug like '#{params[:slug]}'").
+      to_a.
+      group_by { |i| i['name']}
+    @shop_keys = @shops.keys
     render 'publishers/show'
   end
 end
--- app/views/publishers/show.json.jbuilder
+++ app/views/publishers/show.json.jbuilder
@@ -1,12 +1,13 @@
 json.shops do
-	json.array! @shops do |shop|
-		json.id shop.id
-		json.name shop.name
+	json.array! @shop_keys do |shop_key|
+		json.id @shops[shop_key][0]["shop_id"]
+		json.name shop_key
 		json.books_sold_count
 		json.books_in_stock do
-			json.array! shop.books do |book|
-				json.id book.id
-				json.title book.title
+			json.array! @shops[shop_key] do |book|
+				json.id book['book_id']
+				json.title book['title']
+				json.copies_in_stock book['amount']
 			end
 		end
 	end
